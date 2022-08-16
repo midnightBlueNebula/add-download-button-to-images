86401db1207beb61a85db10f343af98330b7045a
Onur Kaya <46904292+midnightBlueNebula@users.noreply.github.com>
2022-08-16 15:39:36 +0300

    Update add-download-button-to-images.user.js

--- add-download-button-to-images.user.js
+++ add-download-button-to-images.user.js
@@ -6,7 +6,6 @@
 // @author       midnightBlueNebula
 // @match        https://*/*
 // @icon         https://iconarchive.com/download/i99352/dtafalonso/android-lollipop/Downloads.ico
-// @grant        none
 // ==/UserScript==
 
 (function() {
@@ -21,6 +20,7 @@
     downloader.style.zIndex = "1000";
     downloader.style.color = "white";
     downloader.style.backgroundColor = "black";
+    downloader.style.textAlign = "center";
     downloader.innerText = "Download";
     document.body.insertBefore(downloader, document.body.firstChild);
 
@@ -31,6 +31,8 @@
         const coords = event.target.getBoundingClientRect();
         downloader.style.left = coords.x + "px";
         downloader.style.top = coords.y + window.scrollY + "px";
+        downloader.style.width = coords.width + "px";
+        downloader.style.fontSize = "1em";
         downloader.download = event.target.title ? event.target.title + " - " + new Date() : new Date();
         downloader.href = event.target.src;
     }
