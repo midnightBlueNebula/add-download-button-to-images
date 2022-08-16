02672c6b1932608fdfe759f451316a728137fbb2
Onur Kaya <46904292+midnightBlueNebula@users.noreply.github.com>
2022-08-16 11:32:08 +0300

    Create add-download-button-to-images.user.js

---
+++ add-download-button-to-images.user.js
@@ -0,0 +1,44 @@
+// ==UserScript==
+// @name         Add Download Button to Images
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  Adds download button to images when cursor moved on an image.
+// @author       midnightBlueNebula
+// @match        https://*/*
+// @icon         https://iconarchive.com/download/i99352/dtafalonso/android-lollipop/Downloads.ico
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    const downloader = document.createElement("a");
+    downloader.href = "";
+    downloader.download = "";
+    downloader.style.position = "absolute";
+    downloader.style.left = "0px";
+    downloader.style.top = "-1000px";
+    downloader.style.zIndex = "1000";
+    downloader.style.color = "white";
+    downloader.style.backgroundColor = "black";
+    downloader.innerText = "Download";
+    document.body.insertBefore(downloader, document.body.firstChild);
+
+    function showDownloader(event){
+        if(event.target == downloader) { return; }
+        if(event.target.tagName.toLowerCase() != "img") { hideDownloader(); return; }
+
+        const coords = event.target.getBoundingClientRect();
+        downloader.style.left = coords.x + "px";
+        downloader.style.top = coords.y + window.scrollY + "px";
+        downloader.download = event.target.title ? event.target.title + " - " + new Date() : new Date();
+        downloader.href = event.target.src;
+    }
+
+    function hideDownloader(){
+        downloader.style.left = "0px";
+        downloader.style.top = "-1000px";
+    }
+
+    document.addEventListener("mouseover", showDownloader);
+})();
